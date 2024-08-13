a7b7e6087db1aada17f58b079a6e7dfd6df5fde0
Raghunath-Vit <raghunathsinghmannu1@gmail.com>
2024-08-13 17:09:43 +0530

    Changed the Normal Form for AddRole to Form made up of using Formik

--- ecommerce/src/Components/RoleForm.js
+++ ecommerce/src/Components/RoleForm.js
@@ -1,24 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-// import './RoleForm.css'
+import { useFormik } from 'formik';
+import { addRoleSchema } from '../schemas';
 
 const RoleForm = () => {
-  const [name, setName] = useState('');
-  const [status, setStatus] = useState(true); 
-  const navigate = useNavigate();
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await axios.post('http://localhost:3000/api/v1/roles', { name, status });
-      console.log('Role added:', response.data);
-      setName('');
-      setStatus(true);
-      alert("Role is Added")
-    } catch (error) {
-      console.error('Error adding role:', error);
-    }
+
+  const { values, touched, errors, handleBlur, handleChange, handleSubmit, setFieldValue } = useFormik({
+    initialValues: {
+      name: '',
+      status: true,
+    },
+    validationSchema: addRoleSchema,
+    onSubmit: async (values, actions) => {
+      try {
+        const response = await axios.post('http://localhost:3000/api/v1/roles', values);
+        console.log('Role added:', response.data);
+        actions.resetForm();
+        alert("Role is Added");
+        
+      } catch (error) {
+        console.error('Error adding role:', error);
+      }
+    },
+  });
+
+  const handleStatusChange = (event) => {
+    const isChecked = event.target.checked;
+    setFieldValue('status', isChecked);
   };
+
   return (
     <div className='outering'>
       <h2 className='hesdingh1'>Add New Role</h2>
@@ -28,21 +38,27 @@ const RoleForm = () => {
           <input
             type="text"
             id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
             required
           />
+          {errors.name && touched.name ? <p>{errors.name}</p> : null}
         </div>
         <div className='flexadd'>
           <label htmlFor="status">Status:</label>
           <input
             type="checkbox"
             id="status"
-            checked={status}
-            onChange={() => setStatus(!status)}
+            name="status"
+            checked={values.status}
+            onChange={handleStatusChange}
+            onBlur={handleBlur}
           />
-           <span className={status ? 'status-active' : 'status-inactive'}>
-            {status ? 'Active' : 'Inactive'}
+          {errors.status && touched.status ? <p>{errors.status}</p> : null}
+          <span className={values.status ? 'status-active' : 'status-inactive'}>
+            {values.status ? 'Active' : 'Inactive'}
           </span>
         </div>
         <button type="submit" className='btss'>Add Role</button>
